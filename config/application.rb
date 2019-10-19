6c53b6abc0bccc7c36d400dbf40c680f933f51bb
tamago3keran <yamataku3831@gmail.com>
2019-10-19 23:44:58 +0900

    時間設定

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module ShuffleLunch
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = "Tokyo"
   end
 end
