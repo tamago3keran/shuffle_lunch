250fbaa110e633f3cf86e38ba32243aa3d79fe8b
yukimurasawa <yuki.murasawa.0826@gmail.com>
2020-01-11 12:08:25 +0900

    fixed #32 issueに関係ない部分の削除

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -22,10 +22,6 @@ class RestaurantsController < ApplicationController
     end
   end
 
-  def edit 
-    @restaurant = Restaurant.find(params[:id])
-  end
-
   private
     def restaurant_params
       params.require(:restaurant).permit(:name, :url, :description)
