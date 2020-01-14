5120bfc76a2e1d6fc2d0ac36955e5c30a06c535d
yukimurasawa <yuki.murasawa.0826@gmail.com>
2020-01-14 18:16:50 +0900

    fixed #45 口コミの表示

--- app/controllers/restaurant_notes_controller.rb
+++ app/controllers/restaurant_notes_controller.rb
@@ -1,12 +1,13 @@
-class RestaurantNoteController < ApplicationController
+class RestaurantNotesController < ApplicationController
   def create
-    restaurant = Restaurant.find(params[:id])
-    @restaurant_notes = restaurant.restaurant_notes.build(restaurant_notes_params)
-    @restaurant_notes.save
+    restaurant = Restaurant.find("5e16c49e9f880b8e21012e6f")
+    @restaurant_note = restaurant.restaurant_notes.new(restaurant_notes_params)
+    @restaurant_note.save
+    redirect_to restaurant_path("5e16c49e9f880b8e21012e6f")
   end
 
   private
     def restaurant_notes_params
-      params.require(:restaurant_notes).permit(:writer_name, :comment)
+      params.require(:restaurant_note).permit(:writer_name, :comment)
     end
 end
