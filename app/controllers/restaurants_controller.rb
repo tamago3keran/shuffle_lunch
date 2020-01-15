0bd157e48df4b582ea3d088ac331adffb860af1e
yukimurasawa <yuki.murasawa.0826@gmail.com>
2020-01-15 11:17:25 +0900

    fixed #45 お店口コミの表示・登録機能

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,7 +5,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
-    @restaurant_notes = @restaurant.restaurant_notes
+    @restaurant_notes = @restaurant.restaurant_notes.all.desc(:created_at).page(params[:page]).per(6)
     @restaurant_note = RestaurantNote.new
   end
 
