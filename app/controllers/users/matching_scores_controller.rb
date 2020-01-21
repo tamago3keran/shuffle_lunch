9a062034d8a10a22ed68b21f0d198ff895cfaa5e
yukimurasawa <yuki.murasawa.0826@gmail.com>
2020-01-21 10:25:01 +0900

    fixed #66 マッチングスコアの並び順・表示の仕方・その他不要箇所の削除

--- app/controllers/users/matching_scores_controller.rb
+++ app/controllers/users/matching_scores_controller.rb
@@ -1,6 +1,6 @@
 class Users::MatchingScoresController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @matching_scores = MatchingScore.where(owner_id: params[:user_id]).to_a
+    @matching_scores = MatchingScore.where(owner_id: params[:user_id]).order(score: :desc)
   end
 end
