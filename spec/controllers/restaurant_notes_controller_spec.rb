36b28b36f9306188deef812c2d0f57811e9314f2
yukimurasawa <yuki.murasawa.0826@gmail.com>
2020-01-23 17:48:17 +0900

    fixed #82 restaurant_notesコントローラーのテスト実装

--- spec/controllers/restaurant_notes_controller_spec.rb
+++ spec/controllers/restaurant_notes_controller_spec.rb
@@ -1,11 +1,10 @@
 require "rails_helper"
 
 describe RestaurantNotesController do
-  let(:restaurant) { Restaurant.create(name: "test", url: "http://hoge.test/", description: "") }
+  let(:restaurant) { Restaurant.create(name: "test", url: "http://example.com/", description: "") }
   let(:restaurant_note_params) {
     { writer_name: "test_name", comment: "example" }
   }
-
 
   describe "create" do
     it "redirects to restaurant show page" do
@@ -16,19 +15,7 @@ describe RestaurantNotesController do
     it "creates a new Restaurant note" do
       expect {
         post :create, params: { restaurant_id: restaurant.id, restaurant_note: restaurant_note_params }
-      }.to change(restaurant.restaurant_notes, :count).by(1)
+      }.to change { Restaurant.find(restaurant.id).restaurant_notes.count }.by(1)
     end
   end
 end
-
-# context “with valid params” do
-#   it “creates a new Restaurant” do
-#     expect {
-#       post :create, params: { restaurant: valid_attributes }
-#     }.to change(Restaurant, :count).by(1)
-#   end
-#   it “redirects to the restaurant index page” do
-#     post :create, params: { restaurant: valid_attributes }
-#     expect(response).to redirect_to restaurants_path
-#   end
-# end
