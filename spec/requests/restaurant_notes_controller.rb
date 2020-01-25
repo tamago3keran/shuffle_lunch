c6cd0cdd217b0fb1c9ba69b38d2770fca68b0eca
yukimurasawa <yuki.murasawa.0826@gmail.com>
2020-01-25 11:19:08 +0900

    fixed #90 口コミ削除機能の実装

--- spec/requests/restaurant_notes_controller.rb
+++ spec/requests/restaurant_notes_controller.rb
@@ -2,11 +2,14 @@ require "rails_helper"
 
 describe RestaurantNotesController do
   let(:restaurant) { Restaurant.create(name: "test", url: "http://example.com/", description: "") }
-  let(:restaurant_note_params) {
-    { writer_name: "test_name", comment: "example" }
-  }
+  let(:restaurant_note) { restaurant.restaurant_notes.create(writer_name: "testtest", comment: "http://hoge.test/test") }
 
-  describe "DELETE /restaurants/:restaurant_id/restaurant_notes/:id" do
-    
-  end
+
+#   describe "DELETE /restaurants/:restaurant_id/restaurant_notes/:id" do
+  
+#     it "redirects to restaurant index page" do
+#       delete restaurant_path(restaurant, restaurant_note) params: { id: restaurant_note.id }
+#       expect(response).to redirect_to restaurant
+#     end
+#   end
 end
