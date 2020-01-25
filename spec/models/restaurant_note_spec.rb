8c1892c45e74d5a79d279d831a5b3299a6983b5f
yukimurasawa <yuki.murasawa.0826@gmail.com>
2020-01-26 00:38:11 +0900

    fixed #77 レビューの修正

--- spec/models/restaurant_note_spec.rb
+++ spec/models/restaurant_note_spec.rb
@@ -1,12 +1,17 @@
 require "rails_helper"
 
 describe RestaurantNote do
-  let(:restaurant_note) { RestaurantNote.new(comment: "test", writer_name: "example") }
+  let(:restaurant_note) { RestaurantNote.new(writer_name: "example", comment: "test") }
+
+  it "is valid with a writer_name and comment" do
+    expect(restaurant_note).to be_valid
+  end
 
   describe "#writer_name" do
     it "is not valid without writer_name" do
       restaurant_note.writer_name = nil
       expect(restaurant_note).not_to be_valid
+      expect(restaurant_note.errors[:writer_name]).to include "can't be blank"
     end
   end
 
@@ -14,18 +19,20 @@ describe RestaurantNote do
     it "is not valid without comment" do
       restaurant_note.comment = nil
       expect(restaurant_note).not_to be_valid
+      expect(restaurant_note.errors[:comment]).to include "can't be blank"
     end
 
     context "when comment is more than 100 characters" do
       it "is not valid" do
-        restaurant_note.comment = "a"*101
+        restaurant_note.comment = "a" * 101
         expect(restaurant_note).not_to be_valid
+        expect(restaurant_note.errors[:comment]).to include "is too long (maximum is 100 characters)"
       end
     end
 
     context "when comment is less than 100 characters" do
       it "is valid" do
-        restaurant_note.comment = "a"*99
+        restaurant_note.comment = "a" * 99
         expect(restaurant_note).to be_valid
       end
     end
