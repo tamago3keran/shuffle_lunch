ee2946481f055c904483c745e03a3f308db07dd6
yukimurasawa <yuki.murasawa.0826@gmail.com>
2020-01-27 09:21:36 +0900

    fixed #74 レビュー修正

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,16 +3,22 @@ require "rails_helper"
 describe Restaurant do
   let(:restaurant) { Restaurant.new(name: "test", url: "http://hoge.test/", description: "testtest") }
 
+  it "is valid with a name, url and email, and description" do
+    expect(restaurant).to be_valid
+  end
+
   describe "#name" do
 
     it "is not valid without name" do
       restaurant.name = nil
       expect(restaurant).not_to be_valid
+      expect(restaurant.errors[:name]).to include "can't be blank"
     end
 
     it "should have a unique name" do
       Restaurant.create(name: "test", url: "http://example.com", description: "")
       expect(restaurant).not_to be_valid
+      expect(restaurant.errors[:name]).to include "is already taken"
     end
   end
 
@@ -21,11 +27,13 @@ describe Restaurant do
     it "is not valid without url" do
       restaurant.url = nil
       expect(restaurant).not_to be_valid
+      expect(restaurant.errors[:url]).to include "can't be blank"
     end
 
     it "should have a unique url" do
       Restaurant.create(name: "test1", url: "http://hoge.test/", description: "")
       expect(restaurant).not_to be_valid
+      expect(restaurant.errors[:url]).to include "is already taken"
     end
 
     context "when invalid format" do
@@ -34,6 +42,7 @@ describe Restaurant do
         invalid_url.each do |url|
           restaurant.url = url
           expect(restaurant).not_to be_valid
+          expect(restuarant.errors[:url]).to include "is invalid"
         end
       end
     end
